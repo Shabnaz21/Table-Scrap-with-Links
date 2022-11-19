ea27a77d0d095fdb6cf1bb867d5ec271ce241468
shabnaz21 <shabnaz608@gmail.com>
2022-11-20 00:46:24 +0600

    second push

--- homepage to single page data scrap.py
+++ homepage to single page data scrap.py
@@ -1,25 +1,23 @@
 from requests import get
 from bs4 import BeautifulSoup as bs
-from csv import writer
+
 url = 'https://www.countrycode.org'
 
 res = get(url)
 soup = bs(res.text, 'html.parser')
 table_body = soup.find('tbody')
-table_row = table_body.find
+table_row = table_body.find_all('a')
 
-link_list = []
-for row in table_body.find_all('a', href=True):
-    link_list.append(url + row['href'])
-
-file = open('countrydata2.csv', 'a+')
-for link in link_list:
-    file.write(f'{link}\n')
+file = open('Url data.csv', 'a+')
+for link in table_row:
+    data = link.get('href')
+    Full_link = (url+data)
+    file.write(Full_link)
+    file.write('\n')
 file.close()
 
 
-# urls = []
-# for url in urls:
+
 
 
 
